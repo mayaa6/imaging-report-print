f6357cdc85d480c0c022ca06ab5df7b7e75fce04
mayaa6 <enarkadia@gmail.com>
2021-02-19 18:30:01 +0800

    make report for B-ultrasonic examine

--- render.js
+++ render.js
@@ -40,7 +40,7 @@ printPDFBtn.addEventListener('click', function (event) {
     const is_form_validated = formValidate()
     console.log(is_form_validated)
     if (is_form_validated) {
-        const file_name = "[" + document.getElementById("dateInput").value + "]" + document.getElementById("nameInput").value.trim()+"_" + document.getElementById("bodyPartInput").value.trim()
+        const file_name = "[" + document.getElementById("dateInput").value + "]" + document.getElementById("nameInput").value.trim()
         ipcRenderer.send('print-to-pdf', file_name)
     } else {
         ipcRenderer.send('form-unfinished')
@@ -66,3 +66,49 @@ ipcRenderer.on('form-reset-result', (event, result) => {
     }
     return
 })
+
+const jpgBtn1 = document.getElementById("jpgFile1")
+const jpgPreview1 = document.getElementById("imageJPG1")
+const jpgRemove1 = document.getElementById("imageRemoveBtn1")
+
+jpgBtn1.onchange = function () {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+        // get loaded data and render thumbnail.
+        jpgPreview1.style.backgroundImage  = "url("+e.target.result+")";
+        jpgPreview1.classList.add("has-image");
+    };
+
+    // read the image file as a data URL.
+    reader.readAsDataURL(this.files[0]);
+};
+
+jpgRemove1.onclick = function(e) {
+    e.preventDefault();
+    jpgPreview1.style.backgroundImage = "none";
+    jpgPreview1.classList.remove("has-image");
+}
+
+const jpgBtn2 = document.getElementById("jpgFile2")
+const jpgPreview2 = document.getElementById("imageJPG2")
+const jpgRemove2 = document.getElementById("imageRemoveBtn2")
+
+jpgBtn2.onchange = function () {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+        // get loaded data and render thumbnail.
+        jpgPreview2.style.backgroundImage  = "url("+e.target.result+")";
+        jpgPreview2.classList.add("has-image");
+    };
+
+    // read the image file as a data URL.
+    reader.readAsDataURL(this.files[0]);
+};
+
+jpgRemove2.onclick = function(e) {
+    e.preventDefault();
+    jpgPreview2.style.backgroundImage = "none";
+    jpgPreview2.classList.remove("has-image");
+}
